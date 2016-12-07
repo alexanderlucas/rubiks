6a9161936933b012276fbd9e99dba740eb4aa579
Alex Lucas <alucas96@me.com>
2016-12-07 17:27:16 -0500

    constructor math

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -10,6 +10,10 @@ public class RubiksCube {
     private static final int ONE = 1;
     private static final int TWO = 2;
 
+    private Color[] colorValues = Color.values();
+    private Face[] faceValues = Face.values();
+
+
     Piece[][][] cube = new Piece[3][3][3];
 
     public RubiksCube(){
@@ -24,6 +28,19 @@ public class RubiksCube {
 
     public Piece[][][] getCube(){
         return cube;
+    }
+
+    public Piece[][][] getCubeCopy(){
+        Piece[][][] cubeCopy = new Piece[3][3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    cubeCopy[i][j][k] = cube[i][j][k];
+                }
+            }
+        }
+        return cubeCopy;
     }
 
     public void showCube(){ //because it prints
@@ -48,247 +65,82 @@ public class RubiksCube {
     }
 
     private void initializeCenterPieces(){
-        cube[ONE][ONE][ZERO] = new CenterPiece(Color.RED, Face.FRONT);
-        cube[ONE][ONE][TWO] = new CenterPiece(Color.GREEN, Face.BACK);
-        cube[ZERO][ONE][ONE] = new CenterPiece(Color.WHITE, Face.LEFT);
+        cube[ONE][ONE][ZERO] = new CenterPiece(Color.WHITE, Face.FRONT);
+        cube[ONE][ONE][TWO] = new CenterPiece(Color.ORANGE, Face.BACK);
+        cube[ONE][ZERO][ONE] = new CenterPiece(Color.RED, Face.TOP);
+        cube[ONE][TWO][ONE] = new CenterPiece(Color.GREEN, Face.BOTTOM);
+        cube[ZERO][ONE][ONE] = new CenterPiece(Color.YELLOW, Face.LEFT);
         cube[TWO][ONE][ONE] = new CenterPiece(Color.BLUE, Face.RIGHT);
-        cube[ONE][ZERO][ONE] = new CenterPiece(Color.YELLOW, Face.TOP);
-        cube[ONE][TWO][ONE] = new CenterPiece(Color.ORANGE, Face.BOTTOM);
     }
 
     private void initializeCornerPieces(){
-        ArrayList<Color> colors = new ArrayList<>();
-        ArrayList<Face> faces = new ArrayList<>();
 
-        //front top left
-        colors.add(Color.RED);
-        colors.add(Color.YELLOW);
-        colors.add(Color.WHITE);
+        for (int l = 0; l < 8; l++) {
+            int i =((l/4)%2) * 2;
+            int j =((l/2)%2) * 2;
+            int k =(l%2) * 2;
 
-        faces.add(Face.FRONT);
-        faces.add(Face.TOP);
-        faces.add(Face.LEFT);
+            ArrayList<Color> colors = getColors(i, j, k);
+            ArrayList<Face> faces = getFaces(i, j, k);
 
-        cube[ZERO][ZERO][ZERO] = new CornerPiece(colors, faces);
-
-        //front top right
-        colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.YELLOW);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.TOP);
-        faces.add(Face.RIGHT);
-
-        cube[TWO][ZERO][ZERO] = new CornerPiece(colors, faces);
-
-        //front bottom left
-        colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.ORANGE);
-        colors.add(Color.WHITE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.BOTTOM);
-        faces.add(Face.LEFT);
-
-        cube[ZERO][TWO][ZERO] = new CornerPiece(colors, faces);
-
-        //front bottom right
-        colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.ORANGE);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.BOTTOM);
-        faces.add(Face.RIGHT);
-
-        cube[TWO][TWO][ZERO] = new CornerPiece(colors, faces);
-
-        //back top left
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.YELLOW);
-        colors.add(Color.WHITE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.TOP);
-        faces.add(Face.LEFT);
-
-        cube[ZERO][ZERO][TWO] = new CornerPiece(colors, faces);
-
-        //back top right
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.YELLOW);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.TOP);
-        faces.add(Face.RIGHT);
-
-        cube[TWO][ZERO][TWO] = new CornerPiece(colors, faces);
-
-        //back bottom left
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.ORANGE);
-        colors.add(Color.WHITE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.BOTTOM);
-        faces.add(Face.LEFT);
-
-        cube[ZERO][TWO][TWO] = new CornerPiece(colors, faces);
-
-        //back bottom right
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.ORANGE);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.BOTTOM);
-        faces.add(Face.RIGHT);
-
-        cube[TWO][TWO][TWO] = new CornerPiece(colors, faces);
+            cube[i][j][k] = new CornerPiece(colors, faces);
+        }
 
     }
 
     private void initializeEdgePieces(){
 
+        for (int l = 0; l < 13; l++) {
+            int odd = l*2 + 1;
 
-        //front top
-        ArrayList<Color> colors= new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.YELLOW);
+            int i =((odd/9)%3);
+            int j =((odd/3)%3);
+            int k =(odd%3);
+
+            if(odd != 13){
+                System.out.println(i+""+j+""+k);
+                ArrayList<Color> colors = getColors(i, j, k);
+                ArrayList<Face> faces = getFaces(i, j, k);
+                cube[i][j][k] = new EdgePiece(colors, faces);
+
+            }
+
+        }
+    }
+
+    private ArrayList<Face> getFaces(int i, int j, int k){
+        Face[] faceValues = Face.values();
+
         ArrayList<Face> faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.TOP);
 
-        cube[ONE][ZERO][ZERO] = new EdgePiece(colors, faces);
+        if (i != 1) {
+            faces.add(faceValues[i/2 + 2]);
+        }
+        if(j != 1){
+            faces.add(faceValues[j/2]);
+        }
+        if(k != 1){
+            faces.add(faceValues[k/2 + 4]);
+        }
 
-        //front left
-        colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.WHITE);
+        return faces;
+    }
 
-        faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.LEFT);
+    private ArrayList<Color> getColors(int i, int j, int k){
+        Color[] colorValues = Color.values();
 
-        cube[ZERO][ONE][ZERO] = new EdgePiece(colors, faces);
+        ArrayList<Color> colors = new ArrayList<>();
+        if(i != 1){
+            colors.add(colorValues[i/2 + 2]);
+        }
+        if(j != 1){
+            colors.add(colorValues[j/2]);
+        }
+        if(k != 1){
+            colors.add(colorValues[k/2 + 4]);
+        }
 
-        //front right
-        colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.RIGHT);
-        cube[TWO][ONE][ZERO] = new EdgePiece(colors, faces);
-
-        //front bottom
-        colors = new ArrayList<>();
-        colors.add(Color.RED);
-        colors.add(Color.ORANGE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.FRONT);
-        faces.add(Face.BOTTOM);
-        cube[ONE][TWO][ZERO] = new EdgePiece(colors, faces);
-
-        //back top
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.YELLOW);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.TOP);
-        cube[ONE][ZERO][TWO] = new EdgePiece(colors, faces);
-
-        //back left
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.WHITE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.LEFT);
-        cube[ZERO][ONE][TWO] = new EdgePiece(colors, faces);
-
-        //back right
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.RIGHT);
-        cube[TWO][ONE][TWO] = new EdgePiece(colors, faces);
-
-        //back bottom
-        colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.ORANGE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BACK);
-        faces.add(Face.BOTTOM);
-        cube[ONE][TWO][TWO] = new EdgePiece(colors, faces);
-
-
-        //top left
-        colors = new ArrayList<>();
-        colors.add(Color.YELLOW);
-        colors.add(Color.WHITE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.TOP);
-        faces.add(Face.LEFT);
-        cube[ZERO][ZERO][ONE] = new EdgePiece(colors, faces);
-
-        //top right
-        colors = new ArrayList<>();
-        colors.add(Color.YELLOW);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.TOP);
-        faces.add(Face.RIGHT);
-        cube[TWO][ZERO][ONE] = new EdgePiece(colors, faces);
-
-        //bottom left
-        colors = new ArrayList<>();
-        colors.add(Color.ORANGE);
-        colors.add(Color.WHITE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BOTTOM);
-        faces.add(Face.LEFT);
-        cube[ZERO][TWO][ONE] = new EdgePiece(colors, faces);
-
-        //bottom right
-        colors = new ArrayList<>();
-        colors.add(Color.ORANGE);
-        colors.add(Color.BLUE);
-
-        faces = new ArrayList<>();
-        faces.add(Face.BOTTOM);
-        faces.add(Face.RIGHT);
-        cube[TWO][TWO][ONE] = new EdgePiece(colors, faces);
-
+        return colors;
     }
 
     private void printLeftTopRight(){
@@ -384,19 +236,6 @@ public class RubiksCube {
         }
     }
 
-
-    public Piece[][][] getCubeCopy(){
-        Piece[][][] cubeCopy = new Piece[3][3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    cubeCopy[i][j][k] = cube[i][j][k];
-                }
-            }
-        }
-        return cubeCopy;
-    }
 
 
 }
